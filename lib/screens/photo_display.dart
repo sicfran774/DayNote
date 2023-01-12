4fbc40b9997d4d8eced22d92236e2ba2e3739454
sicfran774 <sicfran.774@gmail.com>
2023-01-12 14:35:59 -0800

    Edited image preview

--- lib/screens/photo_display.dart
+++ lib/screens/photo_display.dart
@@ -9,25 +9,24 @@ class PhotoDisplay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       backgroundColor: Colors.black,
       appBar: AppBar(title: const Text('Image Preview')),
       // Temp image, must change into path of photo
-      body: Center(
-        child: Image(image: assetImage),
-      ),
-      floatingActionButton: Container(
-        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            FloatingActionButton(
-              child: const Icon(Icons.photo),
-              onPressed: () => ImportImages.openGallery(),
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: assetImage,
+            fit: BoxFit.contain,
+          ),
         ),
       ),
+      floatingActionButton: Container(
+          padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+          child: FloatingActionButton(
+            child: const Icon(Icons.photo),
+            onPressed: () => ImportImages.openGallery(),
+          )),
     );
   }
 }
