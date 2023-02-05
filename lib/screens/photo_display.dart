c123f6cb1474814b9aa851930b5c67dafa2a6f2f
sicfran774 <sicfran.774@gmail.com>
2023-02-04 22:46:15 -0800

    Fixed photo display

--- lib/screens/photo_display.dart
+++ lib/screens/photo_display.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import '../func/import_images.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PhotoDisplay extends StatefulWidget {
   final AssetImage assetImage;
@@ -11,14 +11,14 @@ class PhotoDisplay extends StatefulWidget {
 }
 
 class _PhotoDisplayState extends State<PhotoDisplay> {
+  final ImagePicker _picker = ImagePicker();
   late AssetImage assetImage = widget.assetImage;
-  late String _path = "";
+  XFile? displayImage;
 
-  void changePhoto() {
+  Future getPhoto(ImageSource source) async {
+    XFile? image = await _picker.pickImage(source: source);
     setState(() {
-      ImportImages.openGallery().then((newPath) {
-        _path = newPath;
-      });
+      displayImage = image;
     });
   }
 
@@ -32,32 +32,20 @@ class _PhotoDisplayState extends State<PhotoDisplay> {
             padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
             child: FloatingActionButton(
               child: const Icon(Icons.photo),
-              onPressed: () => changePhoto(),
+              onPressed: () => getPhoto(ImageSource.gallery),
             )),
         body: work());
   }
 
   Widget work() {
-    return SafeArea(
-      child: FutureBuilder(builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text(
-                '${snapshot.error} has occurred',
-              ),
-            );
-          } else if (snapshot.hasData) {
-            return Image.file(File(_path));
-          }
-        }
-        return Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-          image: assetImage,
-          fit: BoxFit.contain,
-        )));
-      }),
-    );
+    return Container(
+        child: displayImage != null
+            ? Image.file(File(displayImage!.path))
+            : Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                image: assetImage,
+                fit: BoxFit.contain,
+              ))));
   }
 }
