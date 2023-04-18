ce100a7c62246ff92467669b3a67c8f2c08f9c23
sicfran774 <sicfran.774@gmail.com>
2023-04-17 22:30:22 -0700

    Start album view

--- lib/screens/components/album/album_daynote.dart
+++ lib/screens/components/album/album_daynote.dart
@@ -1,11 +1,35 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'dart:io';
+
+import 'package:day_note/spec/get_file.dart';
+import 'package:flutter/material.dart';
 
 class AlbumDayNote extends StatelessWidget {
-  const AlbumDayNote({super.key});
+  const AlbumDayNote({super.key, required this.dayNoteList});
+  final List<String> dayNoteList;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    PageController horizontalController = PageController();
+    return Scaffold(
+        appBar: AppBar(),
+        body: PageView(
+          controller: horizontalController,
+          children: [
+            for (String dayNote in dayNoteList) ...[
+              individualDayNote(
+                  dayNote.split('/')[0], int.parse(dayNote.split('/')[1]))
+            ]
+          ],
+        ));
+  }
+
+  Widget individualDayNote(String date, int index) {
+    File image = File(GetFile.path(date, "photo", index: index));
+    return Row(
+      children: [
+        Expanded(
+            child: InteractiveViewer(child: Image(image: FileImage(image)))),
+      ],
+    );
   }
 }
